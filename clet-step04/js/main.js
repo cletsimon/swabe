5882342e1ddd041b690d8e2d86faee4e3aa178f1
Clet Simon Sevillejo <cssevillejo@gmail.com>
2017-04-21 17:54:34 +0800

    socket io create room

--- clet-step04/js/main.js
+++ clet-step04/js/main.js
@@ -1,189 +1,32 @@
 'use strict';
 
-var localConnection;
-var remoteConnection;
-var sendChannel;
-var receiveChannel;
-var pcConstraint;
-var dataConstraint;
-var dataChannelSend = document.querySelector('textarea#dataChannelSend');
-var dataChannelReceive = document.querySelector('textarea#dataChannelReceive');
-var startButton = document.querySelector('button#startButton');
-var sendButton = document.querySelector('button#sendButton');
-var closeButton = document.querySelector('button#closeButton');
+var isInitiator;
 
-startButton.onclick = createConnection;
-sendButton.onclick = sendData;
-closeButton.onclick = closeDataChannels;
+window.room = prompt("Enter room name:");
 
-function enableStartButton() {
-  startButton.disabled = false;
+var socket = io.connect();
+
+if (room !== "") {
+  console.log('Message from client: Asking to join room ' + room);
+  socket.emit('create or join', room);
 }
 
-function disableSendButton() {
-  sendButton.disabled = true;
-}
+socket.on('created', function(room, clientId) {
+  isInitiator = true;
+});
 
-function createConnection() {
-  dataChannelSend.placeholder = '';
-  var servers = null;
-  pcConstraint = null;
-  dataConstraint = null;
-  trace('Using SCTP based data channels');
-  // For SCTP, reliable and ordered delivery is true by default.
-  // Add localConnection to global scope to make it visible
-  // from the browser console.
-  window.localConnection = localConnection =
-      new RTCPeerConnection(servers, pcConstraint);
-  trace('Created local peer connection object localConnection');
+socket.on('full', function(room) {
+  console.log('Message from client: Room ' + room + ' is full :^(');
+});
 
-  sendChannel = localConnection.createDataChannel('sendDataChannel',
-      dataConstraint);
-  trace('Created send data channel');
+socket.on('ipaddr', function(ipaddr) {
+  console.log('Message from client: Server IP address is ' + ipaddr);
+});
 
-  localConnection.onicecandidate = iceCallback1;
-  sendChannel.onopen = onSendChannelStateChange;
-  sendChannel.onclose = onSendChannelStateChange;
+socket.on('joined', function(room, clientId) {
+  isInitiator = false;
+});
 
-  // Add remoteConnection to global scope to make it visible
-  // from the browser console.
-  window.remoteConnection = remoteConnection =
-      new RTCPeerConnection(servers, pcConstraint);
-  trace('Created remote peer connection object remoteConnection');
-
-  remoteConnection.onicecandidate = iceCallback2;
-  remoteConnection.ondatachannel = receiveChannelCallback;
-
-  localConnection.createOffer().then(
-    gotDescription1,
-    onCreateSessionDescriptionError
-  );
-  startButton.disabled = true;
-  closeButton.disabled = false;
-}
-
-function onCreateSessionDescriptionError(error) {
-  trace('Failed to create session description: ' + error.toString());
-}
-
-function sendData() {
-  var data = dataChannelSend.value;
-  sendChannel.send(data);
-  trace('Sent Data: ' + data);
-}
-
-function closeDataChannels() {
-  trace('Closing data channels');
-  sendChannel.close();
-  trace('Closed data channel with label: ' + sendChannel.label);
-  receiveChannel.close();
-  trace('Closed data channel with label: ' + receiveChannel.label);
-  localConnection.close();
-  remoteConnection.close();
-  localConnection = null;
-  remoteConnection = null;
-  trace('Closed peer connections');
-  startButton.disabled = false;
-  sendButton.disabled = true;
-  closeButton.disabled = true;
-  dataChannelSend.value = '';
-  dataChannelReceive.value = '';
-  dataChannelSend.disabled = true;
-  disableSendButton();
-  enableStartButton();
-}
-
-function gotDescription1(desc) {
-  localConnection.setLocalDescription(desc);
-  trace('Offer from localConnection \n' + desc.sdp);
-  remoteConnection.setRemoteDescription(desc);
-  remoteConnection.createAnswer().then(
-    gotDescription2,
-    onCreateSessionDescriptionError
-  );
-}
-
-function gotDescription2(desc) {
-  remoteConnection.setLocalDescription(desc);
-  trace('Answer from remoteConnection \n' + desc.sdp);
-  localConnection.setRemoteDescription(desc);
-}
-
-function iceCallback1(event) {
-  trace('local ice callback');
-  if (event.candidate) {
-    remoteConnection.addIceCandidate(
-      event.candidate
-    ).then(
-      onAddIceCandidateSuccess,
-      onAddIceCandidateError
-    );
-    trace('Local ICE candidate: \n' + event.candidate.candidate);
-  }
-}
-
-function iceCallback2(event) {
-  trace('remote ice callback');
-  if (event.candidate) {
-    localConnection.addIceCandidate(
-      event.candidate
-    ).then(
-      onAddIceCandidateSuccess,
-      onAddIceCandidateError
-    );
-    trace('Remote ICE candidate: \n ' + event.candidate.candidate);
-  }
-}
-
-function onAddIceCandidateSuccess() {
-  trace('AddIceCandidate success.');
-}
-
-function onAddIceCandidateError(error) {
-  trace('Failed to add Ice Candidate: ' + error.toString());
-}
-
-function receiveChannelCallback(event) {
-  trace('Receive Channel Callback');
-  receiveChannel = event.channel;
-  receiveChannel.onmessage = onReceiveMessageCallback;
-  receiveChannel.onopen = onReceiveChannelStateChange;
-  receiveChannel.onclose = onReceiveChannelStateChange;
-}
-
-function onReceiveMessageCallback(event) {
-  trace('Received Message');
-  dataChannelReceive.value = event.data;
-}
-
-function onSendChannelStateChange() {
-  var readyState = sendChannel.readyState;
-  trace('Send channel state is: ' + readyState);
-  if (readyState === 'open') {
-    dataChannelSend.disabled = false;
-    dataChannelSend.focus();
-    sendButton.disabled = false;
-    closeButton.disabled = false;
-  } else {
-    dataChannelSend.disabled = true;
-    sendButton.disabled = true;
-    closeButton.disabled = true;
-  }
-}
-
-function onReceiveChannelStateChange() {
-  var readyState = receiveChannel.readyState;
-  trace('Receive channel state is: ' + readyState);
-}
-
-function trace(text) {
-  if (text[text.length - 1] === '\n') {
-    text = text.substring(0, text.length - 1);
-  }
-  if (window.performance) {
-    var now = (window.performance.now() / 1000).toFixed(3);
-    console.log(now + ': ' + text);
-  } else {
-    console.log(text);
-  }
-}
+socket.on('log', function(array) {
+  console.log.apply(console, array);
+});
