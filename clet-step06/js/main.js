3e2a077275de128c58a7df3a2fa307d20ba08ba7
Clet Simon Sevillejo <cssevillejo@gmail.com>
2017-04-21 19:07:24 +0800

    enable stun server

--- clet-step06/js/main.js
+++ clet-step06/js/main.js
@@ -4,11 +4,11 @@
 * Initial setup
 ****************************************************************************/
 
-// var configuration = {
-//   'iceServers': [{
-//     'urls': 'stun:stun.l.google.com:19302'
-//   }]
-// };
+var configuration = {
+  'iceServers': [{
+    'urls': 'stun:stun.l.google.com:19302'
+  }]
+};
 
 var configuration = null;
 
